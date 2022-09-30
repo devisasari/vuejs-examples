e2aaee1b6177636bf51c98dd5f06c14282bc2ac1
devisasari <dev.isasari@yandex.com>
2022-09-30 23:40:31 +0300

    first commit

---
+++ 1-hello-world/src/App.vue
@@ -0,0 +1,13 @@
+<!-- Say Hello World with Vue! -->
+
+<script setup>
+import { ref } from 'vue'
+
+// A "ref" is a reactive data source that stores a value.
+// It's like a "let" in JavaScript.
+const message = ref('Hello World!')
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
